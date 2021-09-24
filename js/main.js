2a42bf140ec63e818eabf0010ef633abc080dd2f
development-nerdmedia <89048842+development-nerdmedia@users.noreply.github.com>
2021-09-24 18:14:24 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,51 +1,3 @@
-// jQuery(document).ready(function ($) {
-
-//     $('#checkbox').change(function(){
-//       setInterval(function () {
-//           moveRight();
-//       }, 3000);
-//     });
-    
-//       var slideCount = $('#slider ul li').length;
-//       var slideWidth = $('#slider ul li').width();
-//       var slideHeight = $('#slider ul li').height();
-//       var sliderUlWidth = slideCount * slideWidth;
-      
-//     //   $('#slider').css({ width: slideWidth, height: slideHeight });
-      
-//     //   $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-      
-//       $('#slider ul li:last-child').prependTo('#slider ul');
-  
-//       function moveLeft() {
-//           $('#slider ul').animate({
-//               left: + slideWidth
-//           }, 1000, function () {
-//               $('#slider ul li:last-child').prependTo('#slider ul');
-//               $('#slider ul').css('left', '');
-//           });
-//       };
-  
-//       function moveRight() {
-//           $('#slider ul').animate({
-//               left: - slideWidth
-//           }, 1000, function () {
-//               $('#slider ul li:first-child').appendTo('#slider ul');
-//               $('#slider ul').css('left', '');
-//           });
-//       };
-  
-//       $('a.control_prev').click(function () {
-//           moveLeft();
-//       });
-  
-//       $('a.control_next').click(function () {
-//           moveRight();
-//       });
-  
-//   });    
-  
-
 var navToggler = document.querySelector(".contact");
 navToggler.addEventListener("click", toggleNav);
 
@@ -108,3 +60,60 @@ function toggleNav(){
     navToggler.classList.toggle("activo");
     document.querySelector(".contacto").classList.toggle("open");
 }
+
+// $('.slidernew').slick({
+//     dots: true,
+//     infinite: true,
+//     autoplay: true,
+//     autoplaySpeed: 6000,
+//     speed: 800,
+//     slidesToShow: 1,
+//     adaptiveHeight: true
+//   });
+
+//   $('.slidernew').slick();
+
+/* carrusel de imagenes equipo administrativo */
+const slider = document.querySelector("#slider1");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnRight = document.querySelector("#btn-right");
+const btnLeft = document.querySelector("#btn-left");
+
+slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnLeft.addEventListener('click', function(){
+    Next();
+});
+
+btnRight.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000);
