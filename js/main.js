bc9ad5ab72a3c928dae473658564fdc3fb6e52ee
development-nerdmedia <89048842+development-nerdmedia@users.noreply.github.com>
2021-09-28 18:15:20 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -192,16 +192,16 @@ $(document).ready(function(){
     $('.responsive').slick({
         dots: true,
         infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         adaptiveHeight: true,
         responsive: [
           {
-            breakpoint: 1024,
+            breakpoint: 1025,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 2,
+              slidesToScroll: 2,
               infinite: true,
               dots: true
             }
@@ -220,9 +220,6 @@ $(document).ready(function(){
               slidesToScroll: 1
             }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
       });
     $('.slider-equipo').slick();
