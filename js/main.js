1d1faeadb4360ba1da4805e7a357d24166798771
development-nerdmedia <89048842+development-nerdmedia@users.noreply.github.com>
2021-09-30 13:14:23 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -246,5 +246,22 @@ $(document).ready(function(){
           }
         ]
       });
-    $('.slider-equipo').slick();
+      $('.slider-equipo').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 581,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          }
+        ]
+      });
   });
